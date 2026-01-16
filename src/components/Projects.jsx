b54e762c79f8757d9676a7dfe75ca369cc485ecb
Bbuluc <baranbuluc@gmail.com>
2026-01-16 13:09:51 +0100

    added new project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import ProjectItem from "./ProjectItem";
 
 import TicImg from "/images/titanic.jpg";
+import NoteImg from "/images/noteBook.jpg";
 
 export default function Projects() {
   return (
@@ -15,6 +16,14 @@ export default function Projects() {
         link="https://titanic-prediction-seven.vercel.app"
         github="https://github.com/Bbuluc/titanic-prediction"
         tech={["React", "Tailwind CSS", "FastAPI", "Python", "scikit-learn"]}
+      />
+      <ProjectItem
+        image={NoteImg}
+        title="Notes Website"
+        description="A full-stack notes application built with Next.js and SQLite, featuring custom authentication, session management, and user-scoped CRUD operations. Server Actions are used to handle database logic directly within the application. The frontend is styled with Tailwind CSS, providing a responsive and user-friendly interface for managing notes. Live demo and GitHub repository will be added soon."
+        link="https://github.com/Bbuluc/notes-app/blob/main/README.md"
+        github="https://github.com/Bbuluc/notes-app/blob/main/README.md"
+        tech={["Next.js", "Tailwind CSS", "SQLite","Auth/validation"]}
       />
     </div>
   );
